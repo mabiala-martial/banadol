bd1e9ee034bca5725d305256f5642a2ee9103c3e
mabiala-martial <gmabiala@yahoo.fr>
2019-03-07 14:17:39 +0100

    ccueil Missions terminÃ©

--- js/script.js
+++ js/script.js
@@ -12,4 +12,21 @@ $(function(){
 ╚════██║██╔══╝  ██║        ██║   ██║██║   ██║██║╚██╗██║    ██║╚██╔╝██║██║╚════██║╚════██║██║██║   ██║██║╚██╗██║╚════██║
 ███████║███████╗╚██████╗   ██║   ██║╚██████╔╝██║ ╚████║    ██║ ╚═╝ ██║██║███████║███████║██║╚██████╔╝██║ ╚████║███████║
 ╚══════╝╚══════╝ ╚═════╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝    ╚═╝     ╚═╝╚═╝╚══════╝╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝*/
+let menubtns = document.querySelectorAll("[id^=tab_]") /*je récupère tous les éléments dont l'id commence par tab_*/
+menubtns.forEach(e=>{
+	e.addEventListener("click",showMissions)
+});
+function showMissions(evt){
+	
+	let cible = evt.target.parentElement.id.split("_")[1]
+	$('[id^=mission-]').addClass('mission_inactive').removeClass('mision_active');
+	$("#"+cible).toggleClass("mission_active")
+	$("#"+cible).toggleClass("mission_inactive")
+	return false;
+};
+	$('#missions_banadol li').on('click',function(){
+		$('#missions_banadol li').removeClass('active-color');
+		$(this).toggleClass('active-color');
 
+		return false;
+	});
